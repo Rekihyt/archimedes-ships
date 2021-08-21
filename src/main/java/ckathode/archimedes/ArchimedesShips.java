d2e4c4c21e593db4d77b382602220d41ade01bd0
Rekihyt <checkmateguy@gmail.com>
2021-08-20 21:04:58 -0700

    make simplest example build, finally

---
+++ src/main/java/ckathode/archimedes/ArchimedesShips.java
@@ -0,0 +1,49 @@
+package ckathode.archimedes;
+
+import java.util.Collections;
+
+// import net.minecraft.block.Block;
+// import net.minecraft.block.material.Material;
+// import net.minecraft.item.ItemStack;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.common.MinecraftForge;
+import org.apache.logging.log4j.Logger;
+
+// import ckathode.archimedes.blockitem.BlockAS;
+// import ckathode.archimedes.blockitem.BlockCrate;
+// import ckathode.archimedes.blockitem.BlockEngine;
+// import ckathode.archimedes.blockitem.BlockGauge;
+// import ckathode.archimedes.blockitem.BlockHelm;
+// import ckathode.archimedes.blockitem.BlockSeat;
+// import ckathode.archimedes.blockitem.ItemGaugeBlock;
+// import ckathode.archimedes.blockitem.TileEntityCrate;
+// import ckathode.archimedes.blockitem.TileEntityEngine;
+// import ckathode.archimedes.blockitem.TileEntityGauge;
+// import ckathode.archimedes.blockitem.TileEntityHelm;
+// import ckathode.archimedes.command.CommandASHelp;
+// import ckathode.archimedes.command.CommandDisassembleNear;
+// import ckathode.archimedes.command.CommandDisassembleShip;
+// import ckathode.archimedes.command.CommandShipInfo;
+// import ckathode.archimedes.entity.EntityEntityAttachment;
+// import ckathode.archimedes.entity.EntityParachute;
+// import ckathode.archimedes.entity.EntitySeat;
+// import ckathode.archimedes.entity.EntityShip;
+// import ckathode.archimedes.mrot.MetaRotations;
+// import ckathode.archimedes.network.ASMessagePipeline;
+@Mod("archimedesships")
+object ArchimedesShips {
+    
+}
+/*
+@Mod("archimedesships")
+public class ArchimedesShips {
+
+    public ArchimedesShips() {
+        // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
+*/
